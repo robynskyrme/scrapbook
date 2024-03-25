6d6152c5d80d05f7caac67b40698695ac1cc943d
robynskyrme <robynskyrme@gmail.com>
2024-03-25 15:41:46 +0000

    Little program to figure out what numbers can be expressed in binary with only a limited set of 1s and 0s

---
+++ RestrictedBinary.py
@@ -0,0 +1,43 @@
+# 25.3.2024
+# Count in binary with only certain numbers of zeros or ones available, showing which integers can and can't be made
+
+def restrictedBinary(o,z):
+
+    limit = (2 ** (o+z+1)) - (2 ** z+1)
+
+    sequence = []
+
+    limit += 1
+
+    for count in range(limit):
+        if binarypossible(count,o,z):
+            sequence.append(count)
+
+    return sequence
+
+
+def binarypossible(n,o,z):
+    binstr = bin(n)[2:]
+    # print(binstr)
+
+    if len(binstr) > o+z:
+        return False
+
+    if binstr.count("0") > z:
+        return False
+
+    if binstr.count("1") > o:
+        return False
+
+    print(binstr + " = " + str(n))
+    return True
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    seq = (restrictedBinary(11,4))
+    #print (seq)
