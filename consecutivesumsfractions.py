bcf5c03cb9893a3a82c7ea339cd1879427b680a7
robynskyrme <robynskyrme@gmail.com>
2024-06-26 14:07:19 +0100

    Generating a strange pattern from the non-integer parts of a series

--- consecutivesumsfractions.py
+++ consecutivesumsfractions.py
@@ -35,7 +35,7 @@ def farey_vals(low, high,mn):
             print(strx.rjust(7, " ") + bar.rjust(int(width*(m/n))," "))
 
 
-def add_frac_vals(low, high,mn):
+def add_frac_vals(low, high):
     for x in range(low,high+1):
         fracval = 0
         a = x
@@ -59,8 +59,61 @@ def add_frac_vals(low, high,mn):
         #print((str(m)) + "/" + str(n))
         print(fracval)
 
+def count_integer_divs(low, high):
+    max = 0
+    for x in range(low,high+1):
+        a = x
+        b = 1
+        m = n = 0
+
+        count = 0
+
+        while a > 0:
+            if a % b == 0:
+                count += 1
+            else:
+                m += a % b
+                n += b
+                '''print(m)
+                print(n)'''
+
+            #print((str(a)) + "/" + str(b) + int_str)
+            a -= b
+            b += 1
+
+        if count > max:
+            max = count
+            print(max)
+
+
+def return_only_x_with_k_integer_divs(low, high,k):
+    for x in range(low,high+1):
+        a = x
+        b = 1
+        m = n = 0
+
+        count = 0
+
+        while a > 0:
+            if a % b == 0:
+                count += 1
+            else:
+                m += a % b
+                n += b
+                '''print(m)
+                print(n)'''
+
+            #print((str(a)) + "/" + str(b) + int_str)
+            a -= b
+            b += 1
+
+        if count == k:
+            print(x)
+#            print("             " + str(count))
 
 
 if __name__ == "__main__":
-     farey_vals(1,12345,3)
-     #add_frac_vals(1,1234,2)
+     #farey_vals(1000000,1010000,2)
+     #add_frac_vals(1,15)
+     count_integer_divs(1 ,250000)
+     #return_only_x_with_k_integer_divs(1,10000,7)
